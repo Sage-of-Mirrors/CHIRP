5ec908ff4699a19c51fa102c9ba6cdd086715d62
Dylan Ascencio <empressglatia@gmail.com>
2026-09-24 21:15:41 +0100

    Initial project setup

---
+++ projects/chirp/src/main.cpp
@@ -0,0 +1,16 @@
+#include <filesystem>
+#include <iostream>
+
+void PrintTitle()
+{
+  std::cout << "Character Intermediate Representation Pipeline (CHIRP) for "
+               "GX/Flipper." << std::endl;
+  std::cout << "Designed & written by Antidote, Gamma, et al." << std::endl;
+  std::cout << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    PrintTitle();
+	return 0;
+}
